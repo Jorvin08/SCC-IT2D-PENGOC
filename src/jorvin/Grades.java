01806bbe52106924604986b333a39d19086e1541
Jorvin08 <pengocjorvin@gmail.com>
2024-09-20 14:04:38 +0800

    Prelim Repo

---
+++ src/jorvin/Grades.java
@@ -0,0 +1,30 @@
+package jorvin;
+
+public class Grades {
+
+int id;
+String name;
+double p, m, pf, f;
+
+public void addGrades(int sid, String sname, double sp, double sm, double spf, double sf) {
+this.id = sid;
+this.name = sname;
+this.p = sp;
+this.m = sm;
+this.pf = spf;
+this.f = sf;
+}
+
+public void viewGrades() {
+// Correct the average calculation
+double average = (this.p + this.m + this.pf + this.f) / 4;
+
+// Correct the logic for remarks
+String remarks = (average <= 3.0) ? "Passed" : "Failed";
+
+// Correct the formatting string and fix syntax errors
+System.out.printf("%-10d %-10s %-10.2f %-10.2f %-10.2f %-10.2f %-10.2f %-10s\n",
+this.id, this.name, this.p, this.m, this.pf, this.f, average, remarks);
+}
+}
+
---
+++ src/jorvin/main.java
@@ -0,0 +1,16 @@
+
+package jorvin;
+
+public class main {
+    public static void main(String[] args) {
+        
+        //Account acc=new Account();
+        //acc.Input();
+        
+        //product pp = new product();
+        //pp.manageProduct();
+        
+        //Grade gr= new Grade();
+        //gr.grade();
+    }
+}
